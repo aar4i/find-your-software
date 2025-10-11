e392941247ed14c6f0e081ddefa9d540305f6550
aar4i <artyomiv.codes@gmail.com>
2025-10-11 14:39:49 +0200

    Add Tailwind CSS build configuration

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./flaskr/templates/**/*.html",
+    "./flaskr/static/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
